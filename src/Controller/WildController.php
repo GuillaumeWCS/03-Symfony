5649addaa8d242f03746eb67d999effbfc613841
GuillaumeWCS <skrguillaume@gmail.com>
2019-11-20 12:00:21 +0100

    first commit quete 5

--- src/Controller/WildController.php
+++ src/Controller/WildController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use phpDocumentor\Reflection\Location;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,5 +15,16 @@ class WildController extends AbstractController
     public function index(): Response
     {
         return $this->render("wild/index.html.twig", ["website" => "Bienvenue sur Wild Series"]);
+    }
+
+    /**
+     * @Route ("/wild/show/{slug}", requirements={"slug"="[a-z0-9-]+"}, name="wild_show")
+     */
+    // return bien une 404 si les requirements ne sont pas validés
+    public function show(string $slug): Response
+    {
+        $newStr = str_replace("-", " ",$slug);
+        $newStr = ucwords($newStr);
+        return $this->render("wild/show.html.twig", ["parameter" => $newStr]);
     }
 }
