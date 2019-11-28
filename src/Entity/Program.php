6c8e4cef6b6e9189ea0bcfabed6547e7aca8e0c5
GuillaumeWCS <skrguillaume@gmail.com>
2019-11-28 17:06:06 +0100

    change program.php & category.php

--- src/Entity/Program.php
+++ src/Entity/Program.php
@@ -32,10 +32,11 @@ class Program
     private $poster;
 
     /**
-     * @ORM\ManyToOne(targetEntity="App\Entity\Category")
+     * @ORM\ManyToOne(targetEntity="App\Entity\Category", inversedBy="programs")
      * @ORM\JoinColumn(nullable=false)
      */
     private $category;
+
 
     public function getId(): ?int
     {
