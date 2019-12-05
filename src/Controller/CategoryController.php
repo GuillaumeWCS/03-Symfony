f197ee6e86beb3f916d0ced99f7369c838f585b3
GuillaumeWCS <skrguillaume@gmail.com>
2019-12-05 15:56:32 +0100

    add to doctrine category

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -4,8 +4,12 @@ namespace App\Controller;
 
 use App\Entity\Category;
 use App\Form\CategoryType;
+use App\Repository\CategoryRepository;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -13,13 +17,23 @@ class CategoryController extends AbstractController
 {
     /**
      * @Route("/wild/category/add", name="category_add")
+     * @param Request $request
+     * @param EntityManagerInterface $entityManager
      * @return Response A response instance
      */
-    public function add(): Response
+    public function add(Request $request, EntityManagerInterface $entityManager): Response
     {
         $category = new Category();
-        $form = $this->createForm(CategoryType::class, $category)
-            ->add("submit", SubmitType::class);
+        $form = $this
+            ->createForm(CategoryType::class, $category);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($category); // persist un objet pas le form
+            $entityManager->flush();
+        }
 
         return $this->render("category/add.html.twig", [
             "form" => $form->createView(),
